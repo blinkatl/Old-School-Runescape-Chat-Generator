253de204e43bb037677b00dd2c07b4ebde2aeb4e
Daniel Balolong <daniel_balolong@sbcglobal.net>
2024-08-12 04:28:56 -0700

    Create main container and adjust css

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,18 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const path = require('path');
 const chatHeadRouter = require('./routes/chatheadRouter');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 app.use('/chathead', express.static(path.join(__dirname, 'chatheads')));
 app.use('/chathead', chatHeadRouter);
 
 app.get('/', (req, res) => {
     res.json('Hello from the backend!');
   });
-
-
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
